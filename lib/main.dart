04543790c2325391fc7a6a93a93898c31a7747ec
jhon <91237764+RuberDucky@users.noreply.github.com>
2024-10-29 12:31:30 +0500

    chore: Update firebase_auth and firebase_core dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:admin/constants.dart';
 import 'package:admin/controllers/menu_app_controller.dart';
+import 'package:admin/firebase_options.dart';
 import 'package:admin/screens/main/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+    await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
